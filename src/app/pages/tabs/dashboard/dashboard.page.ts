d6da32477c29021ebc6a450b0501d1927a294b8a
vishwanath1004 <50612405+vishwanath1004@users.noreply.github.com>
2025-09-08 00:49:53 +0530

    QA issue : #3479

--- src/app/pages/tabs/dashboard/dashboard.page.ts
+++ src/app/pages/tabs/dashboard/dashboard.page.ts
@@ -97,7 +97,7 @@ export class DashboardPage implements OnInit {
 
   downloadCSV(data){
     try {
-      console.log(this.isMobile," ---- Downloading CSV 100:", data);
+      console.log(this.isMobile," ---- Downloading CSV 100:", data.url,"100 --->", data.fileName);
       if (this.isMobile && (window as any).FlutterChannel) {
       console.log("Downloading CSV 102:", data);
         (window as any).FlutterChannel.postMessage({
