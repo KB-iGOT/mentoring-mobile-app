23742fe0d645b93cd58e36218dae92c009348df8
vishwanath1004 <50612405+vishwanath1004@users.noreply.github.com>
2025-09-15 09:03:53 +0530

    QA issue fix : 2580

--- src/app/pages/tabs/dashboard/dashboard.page.ts
+++ src/app/pages/tabs/dashboard/dashboard.page.ts
@@ -98,6 +98,10 @@ export class DashboardPage implements OnInit {
   downloadCSV(data){
     try {
       console.log(this.isMobile," ---- Downloading CSV 100:", data.url,"100 --->", data.fileName);
+      if (data.fileName?.toLowerCase().endsWith('.csv')) {
+        data.fileName = data.fileName.replace(/\.csv$/i, '');
+      }
+      console.log(data.url,"104 --->", data.fileName);
       this.utilService.parseAndDownloadCSV(data.url, data.fileName);
     } catch (err) {
       console.error("Error posting message to Flutter:", err);
